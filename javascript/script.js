b93291bc365213ce67321565c444639377f42ada
Daniel Kłys <danielklys7@gmail.com>
2019-04-25 13:43:20 +0200

    add script to hide hamburger on scroll and add section 'train'

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,17 @@
 const burgerMenu = document.querySelectorAll('.navburger')
+const burgerIcon = document.querySelector('.navburger')
 
+
+window.addEventListener('scroll', function () {
+    var scrollY = window.scrollY;
+    console.log(scrollY);
+    if ((930 < scrollY && scrollY < 960) && (!burgerIcon.classList.contains('active'))) {
+        for (i = 0; i < burgerMenu.length; i++) {
+            burgerMenu[i].classList.toggle('active');
+        }
+        console.log('triggered!')
+    }
+})
 
 document.addEventListener('click', function () {
     if (!event.target.classList.contains('navburger')) return;
@@ -9,3 +21,11 @@ document.addEventListener('click', function () {
         }
     }
 })
+
+// document.addEventListener('scroll', function () {
+//     if ((scrollY == 500) && (burgerIcon.classList.contains('active'))) {
+//         for (i = 0; i < burgerMenu.length; i++) {
+//             burgerMenu[i].classList.toggle('active');
+//         }
+//     }
+// })
