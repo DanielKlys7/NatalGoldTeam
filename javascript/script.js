2cd0a85f1606267ef9e04f83666f3ac725d776e5
Daniel Kłys <danielklys7@gmail.com>
2019-09-06 11:46:16 +0200

    add few sections and logic to the site

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,10 @@
 const burgerMenu = document.querySelectorAll('.navburger')
-const burgerIcon = document.querySelector('.navburger')
+
 
 // Funkcja chowająca navbar pod warunkiem scrolla w range'u 910-980px od góry strony.
 window.addEventListener('scroll', function () {
-    var scrollY = window.scrollY;
+    const burgerIcon = document.querySelector('.navburger')
+    let scrollY = window.scrollY;
     if ((910 < scrollY && scrollY < 980) && (!burgerIcon.classList.contains('active'))) {
         for (i = 0; i < burgerMenu.length; i++) {
             burgerMenu[i].classList.toggle('active');
@@ -18,5 +19,32 @@ document.addEventListener('click', function () {
         for (i = 0; i < burgerMenu.length; i++) {
             burgerMenu[i].classList.toggle('active');
         }
+    }
+})
+
+let iswrapDownToggled = false;
+const displayDiv = document.querySelector('.wrapDown')
+const buttonWhen = document.querySelector('.btnWhen')
+const buttonPrice = document.querySelector('.btnPrice')
+const calendar = document.querySelector('.calendar')
+const calendarTable = document.querySelector('.calendarTable')
+
+buttonWhen.addEventListener('click', () => {
+    if (!iswrapDownToggled) {
+        displayDiv.style.height = "60vh"
+        iswrapDownToggled = !iswrapDownToggled
+    } else {
+        displayDiv.style.height = "0vh"
+        iswrapDownToggled = !iswrapDownToggled
+    }
+})
+
+buttonPrice.addEventListener('click', () => {
+    if (!iswrapDownToggled) {
+        displayDiv.style.height = "60vh"
+        iswrapDownToggled = !iswrapDownToggled
+    } else {
+        displayDiv.style.height = "0vh"
+        iswrapDownToggled = !iswrapDownToggled
     }
 })
